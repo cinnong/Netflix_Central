4a2247d882f0068c1c81fa053610d53b28ad9238
Dina Oktafiani <dinaoktafiani04@example.com>
2026-01-24 13:50:07 +0700

    finish

--- run_backend.bat
+++ run_backend.bat
@@ -1,5 +1,13 @@
 @echo off
+REM Set environment variables for PostgreSQL (edit sesuai kebutuhan)
+set PGUSER=postgres
+set PGHOST=localhost
+set PGPASSWORD=dina2004
+set PGDATABASE=netflixdb
+set PGPORT=5432
+set PGSSLMODE=disable
+
 REM Start backend
 pushd %~dp0
-netflix-central.exe
+go run main.go
 popd
